Intrigma/SqlClient

commit 9602271e20a1983cf4f0edf3fb8e18d8538e112c

Perf | avoid boxing of SqlGuid in SqlBuffer (#2306)

src/Microsoft.Data.SqlClient/src/Microsoft/Data/SqlClient/SqlBuffer.cs
@@ -109,7 +109,7 @@ namespace Microsoft.Data.SqlClient
         private bool _isNull;
         private StorageType _type;
         private Storage _value;
-        private object _object;    // String, SqlBinary, SqlCachedBuffer, SqlGuid, SqlString, SqlXml
+        private object _object;    // String, SqlBinary, SqlCachedBuffer, SqlString, SqlXml
 
         internal SqlBuffer()
         {
@@ -815,14 +815,17 @@ namespace Microsoft.Data.SqlClient
                 }
                 else if (StorageType.SqlGuid == _type)
                 {
-                    return IsNull ? SqlGuid.Null : (SqlGuid)_object;
+                    return IsNull ? SqlGuid.Null : new SqlGuid(_value._guid);
                 }
                 return (SqlGuid)SqlValue; // anything else we haven't thought of goes through boxing.
             }
             set
             {
                 Debug.Assert(IsEmpty, "setting value a second time?");
-                _object = value;
+                if (!value.IsNull)
+                {
+                    _value._guid = value.Value;
+                }
                 _type = StorageType.SqlGuid;
                 _isNull = value.IsNull;
             }

src/Microsoft.Data.SqlClient/netcore/src/Microsoft/Data/SqlClient/TdsParser.cs
@@ -5504,7 +5504,7 @@ namespace Microsoft.Data.SqlClient
                     break;
 
                 case SqlDbType.UniqueIdentifier:
-                    nullVal.SqlGuid = SqlGuid.Null;
+                    nullVal.SetToNullOfType(SqlBuffer.StorageType.SqlGuid);
                     break;
 
                 case SqlDbType.Bit:

src/Microsoft.Data.SqlClient/netfx/src/Microsoft/Data/SqlClient/TdsParser.cs
@@ -6310,7 +6310,7 @@ namespace Microsoft.Data.SqlClient
                     break;
 
                 case SqlDbType.UniqueIdentifier:
-                    nullVal.SqlGuid = SqlGuid.Null;
+                    nullVal.SetToNullOfType(SqlBuffer.StorageType.SqlGuid);
                     break;
 
                 case SqlDbType.Bit:

src/Microsoft.Data.SqlClient/src/Microsoft/Data/SqlClient/Server/ValueUtilsSmi.netfx.cs
@@ -152,10 +152,6 @@ namespace Microsoft.Data.SqlClient.Server
                     // special case SqlBinary, 'cause tds parser never sets SqlBuffer to null, just to empty!
                     targetBuffer.SqlBinary = SqlBinary.Null;
                 }
-                else if (SqlBuffer.StorageType.SqlGuid == stype)
-                {
-                    targetBuffer.SqlGuid = SqlGuid.Null;
-                }
                 else
                 {
                     targetBuffer.SetToNullOfType(stype);

commit 07448023cd2e199c2fbbde0e072113b849d2fc53

Fix | bugs in SqlBuffer.get_Guid & get_SqlValue (#TBD)
introduced by PR #2306

src/Microsoft.Data.SqlClient/src/Microsoft/Data/SqlClient/SqlBuffer.cs
@@ -383,7 +383,7 @@ namespace Microsoft.Data.SqlClient
                 }
                 else if (StorageType.SqlGuid == _type)
                 {
-                    return ((SqlGuid)_object).Value;
+                    return _value._guid;
                 }
                 return (Guid)Value;
             }
@@ -972,6 +972,8 @@ namespace Microsoft.Data.SqlClient
                         return SqlSingle;
                     case StorageType.String:
                         return SqlString;
+                    case StorageType.SqlBinary:
+                        return _object;
 
                     case StorageType.SqlCachedBuffer:
                         {
@@ -983,9 +985,8 @@ namespace Microsoft.Data.SqlClient
                             return data.ToSqlXml();
                         }
 
-                    case StorageType.SqlBinary:
                     case StorageType.SqlGuid:
-                        return _object;
+                        return SqlGuid;
 
                     case StorageType.SqlXml:
                         if (_isNull)